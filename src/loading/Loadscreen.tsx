ac5080fac6b531c1a3d86e88502553507f3de18b
이정환 <jung20021023@gmail.com>
2025-05-04 16:56:55 +0900

    [feat] 탭추가

--- src/loading/Loadscreen.tsx
+++ src/loading/Loadscreen.tsx
@@ -4,7 +4,7 @@ import {View, Image} from 'react-native';
 export default function LoadingScreen() {
     return (
         <View className="flex-1 items-center justify-center bg-white p-[10px]">
-            {/* 기봉사 로고 margin은 일단 넣어두긴함 css오류로 돌려보진못함함*/}
+            {/* 기봉사 로고 margin은 일단 넣어두긴함 css오류로 돌려보진못함*/}
             <Image source={require('@/assets/logo.png')} style={{width: 211, height: 81, marginBottom: 24}} resizeMode="contain" />
 
             {/* 로딩 아이콘 */}
