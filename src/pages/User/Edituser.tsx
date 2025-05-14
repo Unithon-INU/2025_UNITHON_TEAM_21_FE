10f33f386010aa8c4d5a17b20503bba79c38c107
이정환 <jung20021023@gmail.com>
2025-05-14 18:00:39 +0900

    [etc]

--- src/pages/User/Edituser.tsx
+++ src/pages/User/Edituser.tsx
@@ -5,16 +5,16 @@ import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 
 export default function UserEdit() {
-    const navigation = useNavigation<StackNavigationProp<any>>();
-    return (
-      <Layout>
+  const navigation = useNavigation<StackNavigationProp<any>>();
+  return (
+    <Layout>
       {/* 상단 */}
-        <View className="flex-row items-center justify-between  pb-7 space-x-2">
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Image source={require('@/assets/navi.png')} className="w-8 h-8" />
-          </TouchableOpacity>
-          <Text className="font-inter font-Inter font-bold text-[20px]">   프로필 수정</Text>
-          </View>
-        </Layout>
-    );
+      <View className="flex-row items-center justify-between  pb-7 space-x-2">
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Image source={require('@/assets/navi.png')} className="w-8 h-8" />
+        </TouchableOpacity>
+        <Text className="font-inter font-Inter font-bold text-[20px]">   프로필 수정</Text>
+      </View>
+    </Layout>
+  );
 }
--- src/pages/User/Likedvol.tsx
+++ src/pages/User/Likedvol.tsx
@@ -5,16 +5,16 @@ import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 
 export default function UserLikedvol() {
-    const navigation = useNavigation<StackNavigationProp<any>>();
-    return (
-      <Layout>
+  const navigation = useNavigation<StackNavigationProp<any>>();
+  return (
+    <Layout>
       {/* 상단 */}
-        <View className="flex-row items-center pb-7 space-x-2 ">
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Image source={require('@/assets/navi.png')} className="w-8 h-8" />
-          </TouchableOpacity>
-          <Text className="font-inter font-Inter font-bold text-[20px]">   관심 봉사활동</Text>
-          </View>
-        </Layout>
-    );
+      <View className="flex-row items-center pb-7 space-x-2 ">
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Image source={require('@/assets/navi.png')} className="w-8 h-8" />
+        </TouchableOpacity>
+        <Text className="font-inter font-Inter font-bold text-[20px]">   관심 봉사활동</Text>
+      </View>
+    </Layout>
+  );
 }
